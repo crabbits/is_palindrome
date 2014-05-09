c0d4b10dfa9e9d6552eb9fa7cb6519678b6a95dc
Cristian Tountzis <cristian.tountzis@fridaymedia.com.au>
2014-05-09 11:57:59 +1000

    fixed failing tests

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -7,8 +7,8 @@ module Palindrome
     def initialize string
       @string = string.gsub(/[^0-9a-z]/i, '').downcase
       @head_pointer = 0
-      @end_pointer = string.length - 1
-      @palindrome = true
+      @end_pointer = @string.length - 1
+      @truth_value = true
     end
 
     def head_pointer
@@ -19,16 +19,26 @@ module Palindrome
       @end_pointer
     end
 
+    def truth_value
+      @truth_value
+    end
+
+    def truth_value=(string)
+      @truth_value = string
+    end
+
     def string
       @string
     end
 
-    def palindrome
-      @palindrome
-    end
-
-    def palindrome=(boolean)
-      @palindrome = boolean
+    def is_palindrome?
+      truth_value = while head_pointer <= end_pointer
+        break false if at_head != at_end
+	break true if head_pointer == end_pointer
+	increment_head
+        decrement_end
+      end
+      truth_value
     end
 
     def at_head
@@ -53,21 +63,4 @@ module Palindrome
       string[index]
     end
   end
-
-  def self.is_palindrome? string
-    
-    palindrome = Palindrome.new string
-
-    while palindrome.head_pointer <= palindrome.end_pointer
-      if palindrome.at_head != palindrome.at_end
-        palindrome.palindrome = false      
-	return
-      end
-      palindrome.increment_head
-      palindrome.decrement_end
-    end
-
-    puts palindrome.palindrome.to_s
-  end
-
 end
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -4,15 +4,15 @@ describe Palindrome do
   
   describe "is_palindrome?" do
  
-    let(:palindrome) { "Amore, Roma" }
-    let(:not_palindrome) { "Yer a wizard, Frodo' - Darth Vader" }
+    let(:palindrome) { Palindrome::Palindrome.new("Amore, Roma") }
+    let(:not_palindrome) { Palindrome::Palindrome.new("Yer a wizard, Frodo' - Darth Vader") }
 
     it "should return true if the string is a palindrome" do
-      Palindrome.is_palindrome?(palindrome).should be_true
+      palindrome.is_palindrome?.should == true
     end
 
     it "should return false if the string is not a palindrome" do
-      Palindrome.is_palindrome?(not_palindrome).should be_false
+      not_palindrome.is_palindrome?.should == false
     end
   end
 end
